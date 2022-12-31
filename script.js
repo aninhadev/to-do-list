84aa6f2038d1a488295aa3ff7b5359efd84e1fd4
Ana Maria Neves <anamarianevesdev@gmail.com>
2022-12-30 22:23:22 -0300

    dicionando arrays

--- script.js
+++ script.js
@@ -1,5 +1,83 @@
-var NewTask = document.querySelector()
 
-function NewElement(){
+
+
+
+let newTask = document.querySelector("input#item-text");
+let res = document.querySelector("div.res");
+let task = [];
+
+
+
+
+
+function NewElement() {
+  if (newTask.length == 0){
+    window.alert('Desculpa meu parça! n/ Mas o seu texto é invalido!')
+  } else {
+    task.push(Number(newTask.value));
+    let item = document.createElement("p");
+    item.txt = `dddd${newTask.value}`;
+    res.appendChild(item);
+    res.innerHTML = ''
+    
+  }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var res = document.querySelector("div.res");
+
+
+
+
+// function adicionar() {
+//     if (isNumero(num.value) && !inLista(num.value, valores)) {
+//       valores.push(Number(num.value));
+//       let item = document.createElement("option");
+//       item.text = `Valor  ${num.value} adicionado.`;
+//       lista.appendChild(item);
+//       res.innerHTML = ''
+//     } else {
+//       window.alert("Valor inválido ou já encontrado na lista.");
+//     }
+//     num.value = ''
+//     num.focus()
+//   }
+  
+
+
+
+// if (nome.length == 0) {
+//     window.alert('Adicione uma tarefa rapaiz')
+//   } else {
+//     res.innerHTML = `Agora não sei o que fazer com isso [ ${nome} ] `
+//   }
+  
+
